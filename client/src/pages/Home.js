47efbc7d00c57f8fa106d1b8407fcd8778b94af1
yakattak <stevecalebrese@gmail.com>
2022-05-16 13:52:12 -0400

    .4

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import {useQuery} from '@apollo/client';
-import { QUERY_THOUGHTS} from '../utils/queries'
 import ThoughtList from '../components/ThoughtList';
 
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
+
 const Home = () => {
-  // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
   const thoughts = data?.thoughts || [];
-  console.log(thoughts);
 
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
           {loading ? (
-            <div> Loading... </div>
-          ): (
-            <ThoughtList thoughts = {thoughts} title ="Soe feed for thought(s)...."/>
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Feed for Thought(s)..."
+            />
           )}
-          </div>
+        </div>
       </div>
     </main>
   );
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,14 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
 
   type Thought {
     _id: ID
@@ -13,15 +19,6 @@ const typeDefs = gql`
     reactions: [Reaction]
   }
 
-  type User {
-    _id: ID
-    username: String
-    email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
-  }
-
   type Reaction {
     _id: ID
     reactionBody: String
@@ -29,13 +26,26 @@ const typeDefs = gql`
     username: String
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
